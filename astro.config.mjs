f60c52929f57cad842676ff8eeada1a7585c6e1b
bjmcd <113494847+bjmcd@users.noreply.github.com>
2025-08-17 18:57:45 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,17 +19,17 @@ export default defineConfig({
   integrations: [
       starlight({
           title: 'My Docs',
-          social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+          social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/bjmcd' }],
           sidebar: [
               {
-                  label: 'Guides', 
+                  label: 'Math', 
                   items: [
                       // Each item here is one entry in the navigation menu.
                       { label: 'Example Guide', slug: 'guides/example' },
                   ],
               },
               {
-                  label: 'Reference',
+                  label: 'Ansi C',
                   autogenerate: { directory: 'reference' },
               },
           ],
